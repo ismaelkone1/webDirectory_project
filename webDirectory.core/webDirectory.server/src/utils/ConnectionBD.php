60d78a0a9f99f61553f5e3b98cbe05f01ccf193c
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-06-13 10:46:27 +0200

    Connection à la BD

---
+++ webDirectory.core/webDirectory.server/src/utils/ConnectionBD.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace webDirectory\server\utils;
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+class ConnectionBD
+{
+    static function init(string $iniFile)
+    {
+        $db = new DB();
+        $db->addConnection(parse_ini_file($iniFile));
+        $db->setAsGlobal();
+        $db->bootEloquent();
+    }
+}
