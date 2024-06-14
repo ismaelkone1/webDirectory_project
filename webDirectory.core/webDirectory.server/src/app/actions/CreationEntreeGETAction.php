86a268079a5b939b4df430ce0273e2dbf05a4d2a
matheo <matheo.grandjean8@etu.univ-lorraine.fr>
2024-06-14 15:34:55 +0200

    Continuité de la création d'entrée

--- webDirectory.core/webDirectory.server/src/app/actions/CreationEntreeGETAction.php
+++ webDirectory.core/webDirectory.server/src/app/actions/CreationEntreeGETAction.php
@@ -5,17 +5,20 @@ namespace web\directory\app\actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use web\directory\app\utils\CsrfService;
 
 class CreationEntreeGETAction
 {
     public function __invoke(Request $rq, Response $rs): Response
     {
-
+        $csrf = CsrfService::generate();
         $view = Twig::fromRequest($rq);
         return $view->render(
             $rs,
             'CreationEntree.twig',
-            []
+            [
+                'csrf' => $csrf
+            ]
         );
     }
 }
