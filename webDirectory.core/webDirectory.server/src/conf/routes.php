a77b705cc59c177d0df2103d5a49f00b701f2d39
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-06-14 15:24:50 +0200

    Ajout de la ListeEntree

--- webDirectory.core/webDirectory.server/src/conf/routes.php
+++ webDirectory.core/webDirectory.server/src/conf/routes.php
@@ -1,12 +1,16 @@
 <?php 
 declare(strict_types=1);
 
-use web\directory\utils\ConnectionBD;
 use Slim\App;
 use web\directory\app\actions\HomeAction;
+use web\directory\app\actions\ListeEntreeAction;
 
 return function (App $app) : App {
     $app->get('/', HomeAction::class)
         ->setName('home');
+
+    $app->get('/entrees',ListeEntreeAction::class)
+        ->setName('entrees');
+
     return $app;
 };
