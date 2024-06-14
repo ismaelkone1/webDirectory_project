dcb3bc3945ff328fb7d16d736c07bd211db7af54
hugoAssal <hugo.assal1@etu.univ-lorraine.fr>
2024-06-14 15:08:27 +0200

    api finie et commencement de webDirectory.web

---
+++ webDirectory.core/webDirectory.api/src/core/services/entree/ServiceEntree.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace web\directory\api\core\services;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use web\directory\api\core\domain\Entree;
+
+class ServiceEntree implements ServiceEntreeInterface
+{
+
+
+    /**
+     * @return array
+     * Function to get all the entries
+     */
+    public function getAllEntrees(): array
+    {
+        //On gere les erreurs
+        try {
+            //On retourne toutes les entrees
+            return Entree::with('services')->get()->toArray();
+        } catch (ModelNotFoundException $e) {
+            //On retourne une erreur
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function getEntree(int $id): array
+    {
+        try {
+            return Entree::find($id)->toArray();
+        } catch (ModelNotFoundException $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * @param string $nom
+     * @return array
+     */
+    public function getEntreeEnFonctionDuNom(string $nom): array
+    {
+        try {
+            return Entree::where('nom', 'like', '%' . $nom . '%')->with('services')->get()->toArray();
+        } catch (ModelNotFoundException $e) {
+            return ['error' => $e->getMessage()];
+        }
+    }
+}
