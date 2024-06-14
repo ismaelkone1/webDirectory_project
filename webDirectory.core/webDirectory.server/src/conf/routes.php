3b8c835d743e9a9b193f5208deb099cd94b97c4a
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-06-14 09:45:28 +0200

    Ajout des routes, base twig et HomeAction

---
+++ webDirectory.core/webDirectory.server/src/conf/routes.php
@@ -0,0 +1,12 @@
+<?php 
+declare(strict_types=1);
+
+use web\directory\utils\ConnectionBD;
+use Slim\App;
+use web\directory\app\actions\HomeAction;
+
+return function (App $app) : App {
+    $app->get('/', HomeAction::class)
+        ->setName('home');
+    return $app;
+};
