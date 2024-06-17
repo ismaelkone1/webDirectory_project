e9e9e490056c9fc4567501ef609bd95cbfb03c79
matheo <matheo.grandjean8@etu.univ-lorraine.fr>
2024-06-17 09:33:42 +0200

    Ajout de la fonctionnalitée de création d'entrée

--- webDirectory.core/webDirectory.server/src/core/services/Entree/ServiceEntree.php
+++ webDirectory.core/webDirectory.server/src/core/services/Entree/ServiceEntree.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use web\directory\core\domain\Entree;
 use web\directory\core\services\Entree\EntreeNotFoundException;
 
+
 class ServiceEntree implements ServiceEntreeInterface
 {
 
@@ -30,12 +31,46 @@ class ServiceEntree implements ServiceEntreeInterface
         return $tabEntree->toArray();
     }
 
-    public function getServices() : array {
+    public function getServices(): array
+    {
         try {
             $tabServicesEntrees = Entree::with('services')->get();
         } catch (ModelNotFoundException $e) {
             throw new EntreeNotFoundException("Impossible de récupérer les services : " . $e);
         }
         return $tabServicesEntrees->toArray();
+    }
+
+    public function createEntree(array $data): bool
+    {
+        try {
+            $entree = new Entree();
+            $entree->nom = $data['nom'];
+            $entree->prenom = $data['prenom'];
+            $entree->fonction = $data['fonction'];
+            $entree->num_bureau = $data['numBureau'];
+            $entree->email = $data['email'];
+
+            if (isset($_FILES['urlImage']) && $_FILES['urlImage']['error'] === UPLOAD_ERR_OK) {
+                $uploadDir = 'uploadImages/';
+                $uploadFile = $uploadDir . basename($_FILES['urlImage']['name']);
+                if (!file_exists($uploadDir)) {
+                    mkdir($uploadDir, 0777, true);
+                }
+                if (move_uploaded_file($_FILES['urlImage']['tmp_name'], $uploadFile)) {
+                    $entree->url_image = $uploadFile;
+                } else {
+                    throw new \Exception("Échec du téléchargement de l'image");
+                }
+            } else {
+                $entree->url_image = null;
+            }
+
+            $entree->save();
+            return true;
+        } catch (ModelNotFoundException $e) {
+            throw new EntreeNotFoundException("Impossible de créer l'entrée : " . $e);
+        }
+        return false;
     }
 }
