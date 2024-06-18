4999a979a294578cf5b610a15b63df9f21e0fed2
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2024-06-18 15:11:31 +0200

    Ajout CSRF au formulaire

--- webDirectory.core/webDirectory.server/src/app/actions/CreationServicePOSTAction.php
+++ webDirectory.core/webDirectory.server/src/app/actions/CreationServicePOSTAction.php
@@ -5,9 +5,8 @@ namespace web\directory\app\actions;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use web\directory\core\services\Service\ServiceServices;
-use Slim\Exception\HttpNotFoundException;
-use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
+use web\directory\app\utils\CsrfService;
 
 class CreationServicePOSTAction extends Action
 {
@@ -18,15 +17,23 @@ class CreationServicePOSTAction extends Action
         $libelle = htmlspecialchars($data['libelle'], ENT_QUOTES, 'UTF-8');
         $etage = htmlspecialchars($data['etage'], FILTER_SANITIZE_NUMBER_INT, 'UTF-8');
         $description = htmlspecialchars($data['description'], ENT_QUOTES, 'UTF-8');
+        $csrf = htmlspecialchars($data['csrf'], ENT_QUOTES, 'UTF-8');
+
+        $csrf = CsrfService::check($csrf);
+
+        if (!$csrf) {
+            return $rs->withStatus(403);
+        }
 
         $serviceServices = new ServiceServices();
-        $data = [
+        $insert = [
             'libelle' => $libelle,
             'etage' => $etage,
             'description' => $description,
+            
         ];
-
-        if ($serviceServices->createService($data)) {
+    
+        if ($serviceServices->createService($insert)) {
             return $twig->render($rs, 'createServiceSucess.twig');    
         }
     }
