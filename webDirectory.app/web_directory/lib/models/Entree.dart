8f11f71c9789bc00a1e4cab89701fd123e8e15df
matheo <matheo.grandjean8@etu.univ-lorraine.fr>
2024-06-18 10:53:20 +0200

    pull du main

--- webDirectory.app/web_directory/lib/models/Entree.dart
+++ webDirectory.app/web_directory/lib/models/Entree.dart
@@ -10,24 +10,35 @@ class Entree {
       this.urlImage,
       this.numBureau});
 
+  // factory Entree.fromJson(Map<String, dynamic> json) {
+  //   return switch (json) {
+  //     {
+  //       'nom': String nom,
+  //       'prenom': String prenom,
+  //       'fonction': String fonction,
+  //       'email': String email,
+  //       'urlImage': String urlImage,
+  //       'numBureau': int numBureau,
+  //     } =>
+  //       Entree(
+  //           nom: nom,
+  //           prenom: prenom,
+  //           fonction: fonction,
+  //           email: email,
+  //           urlImage: urlImage,
+  //           numBureau: numBureau),
+  //     _ => throw const FormatException('Failed to load entree.'),
+  //   };
+  // }
+
   factory Entree.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'nom': String nom,
-        'prenom': String prenom,
-        'fonction': String fonction,
-        'email': String email,
-        'urlImage': String urlImage,
-        'numBureau': int numBureau,
-      } =>
-        Entree(
-            nom: nom,
-            prenom: prenom,
-            fonction: fonction,
-            email: email,
-            urlImage: urlImage,
-            numBureau: numBureau),
-      _ => throw const FormatException('Failed to load entree.'),
-    };
+    return Entree(
+      nom: json['nom'] as String?,
+      prenom: json['prenom'] as String?,
+      fonction: json['fonction'] as String?,
+      email: json['email'] as String?,
+      urlImage: json['urlImage'] as String?,
+      numBureau: json['numBureau'] as int?,
+    );
   }
 }
