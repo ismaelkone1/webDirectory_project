4d16de21aa366ef8eb3296a761eb1c1c37b1ea55
hugoAssal <hugo.assal1@etu.univ-lorraine.fr>
2024-06-20 11:35:24 +0200

    modifs sql

--- webDirectory.core/sql/schema.sql
+++ webDirectory.core/sql/schema.sql
@@ -1,34 +1,3 @@
-CREATE TABLE entree (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  nom varchar(128),
-  prenom varchar(128),
-  fonction varchar(128),
-  num_bureau int,
-  email varchar(64),
-  url_image varchar(512),
-  created_by VARCHAR(40)
-);
-
-CREATE TABLE telephone (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  id_entree int,
-  numero char(10),
-  type varchar(15)
-);
-
-CREATE TABLE entree_service (
-  id_entree int,
-  id_service int,
-  PRIMARY KEY (id_entree, id_service)
-);
-
-CREATE TABLE service (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  libelle varchar(128),
-  etage smallint,
-  description varchar(512)
-);
-
 CREATE TABLE utilisateur (
   id VARCHAR(40) PRIMARY KEY,
   mail VARCHAR(128),
@@ -48,19 +17,20 @@ CREATE TABLE entree (
   email VARCHAR(64),
   url_image VARCHAR(512),
   is_published BOOLEAN DEFAULT FALSE,
-  created_by VARCHAR(40),
+  created_by VARCHAR(40)
 );
 
 CREATE TABLE telephone (
   id INT PRIMARY KEY AUTO_INCREMENT,
   id_entree INT,
   numero CHAR(10),
+  type varchar(15)
 );
 
 CREATE TABLE entree_service (
   id_entree INT,
   id_service INT,
-  PRIMARY KEY (id_entree, id_service),
+  PRIMARY KEY (id_entree, id_service)
 );
 
 CREATE TABLE service (
@@ -68,11 +38,9 @@ CREATE TABLE service (
   libelle VARCHAR(128),
   etage SMALLINT,
   description VARCHAR(512),
-  created_by VARCHAR(40),
+  created_by VARCHAR(40)
 );
 
-ALTER TABLE utilisateur
-ADD COLUMN activation_token VARCHAR(128);
 
 ALTER TABLE telephone
 ADD CONSTRAINT fk_telephone_entree
